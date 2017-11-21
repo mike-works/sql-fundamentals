a6868319e5a4b998e37e1adc74c166651391153d
Michael North <michael.l.north@gmail.com>
2017-11-20 22:47:59 -1000

    remove commander

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import chalk from 'chalk';
-import * as commander from 'commander';
 import { existsSync } from 'fs';
 import * as path from 'path';
 import { initializeDb } from './db/setup';
@@ -7,10 +6,6 @@ import { startExpressServer } from './express-server';
 
 // tslint:disable-next-line:no-var-requires
 const pkg = require('../package.json');
-
-const program = commander
-  .version(pkg.version)
-  .parse(process.argv);
 
 (async function main() {
   await initializeDb('dev');
