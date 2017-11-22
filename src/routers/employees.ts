79d47c1b0759f97d4b923f02ced98c229e2d8bc1
Michael North <michael.l.north@gmail.com>
2017-11-21 18:32:41 -1000

    bugfix

--- src/routers/employees.ts
+++ src/routers/employees.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   let employees = await getAllEmployees();
-  res.render('customers', { employees });
+  res.render('employees', { employees });
 });
 
 router.get('/:id', async (req, res) => {
