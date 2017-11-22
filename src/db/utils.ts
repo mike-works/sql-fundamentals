f961a271ec11c6b9bd0e45a6f0332603ca6213b2
Michael North <michael.l.north@gmail.com>
2017-11-21 22:11:25 -1000

    create db on the fly if it doesn't exist

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,15 +1,42 @@
+import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as sqlite from 'sqlite';
-import { PROJECT_ROOT } from '../constants';
+import { promisify } from 'util';
+import { MASTER_DB_FILE, PROJECT_ROOT } from '../constants';
+import { logger } from '../log';
+
+async function fileExists(pth: string) {
+  return new Promise(resolve => {
+    fs.exists(pth, resolve);
+  });
+}
 
 export const dbPath = (name: string) =>
   path.join(PROJECT_ROOT, `${name}.sqlite`);
 
 const dbPromises: { [key: string]: Promise<sqlite.Database> } = {};
 
-export function getDb(name: string) {
-  if (!dbPromises[name]) {
-    dbPromises[name] = sqlite.open(dbPath(name));
+export async function getDb(name: string): Promise<sqlite.Database> {
+  let pathToDb = dbPath(name);
+  let doesExist = await fileExists(pathToDb);
+  if (!doesExist) {
+    await initializeDb(name);
   }
-  return dbPromises[name];
+  if (!dbPromises[name]) {
+    dbPromises[name] = sqlite.open(pathToDb);
+  }
+  let db = await dbPromises[name];
+  return db;
+}
+
+const copyFile = promisify(fs.copyFile);
+
+export async function initializeDb(dbName = 'dev') {
+  let pth = dbPath(dbName);
+  if (!fs.existsSync(pth)) {
+    logger.debug(
+      `Database ${dbName} was not found at ${pth}... creating it now`
+    );
+    await copyFile(MASTER_DB_FILE, pth);
+  }
 }
