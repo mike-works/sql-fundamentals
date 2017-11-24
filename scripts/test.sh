0bb2f62a8c536cefb440208842333f4781053c6e
Michael North <michael.l.north@gmail.com>
2017-11-23 14:17:01 -1000

    test filter logging

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 git_branch=$(git symbolic-ref --short -q HEAD)
+echo "Git Branch: $git_branch"
 if [ $1 ]; then
   filter=$1
 else
   filter='BEGIN'
 fi
-echo $filter
-if [ git_branch == 'solutions' ]; then
+echo "Test Filter: $filter"
+if [ $git_branch = 'solutions' ]; then
   NODE_ENV=test ./node_modules/.bin/mocha --require ts-node/register ./tests/*.test.ts
 else 
   NODE_ENV=test ./node_modules/.bin/mocha --grep $filter --require ts-node/register ./tests/*.test.ts
