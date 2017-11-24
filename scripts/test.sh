91bab4dda7a8938f4a648cddd11942119612d1e7
Michael North <michael.l.north@gmail.com>
2017-11-23 14:31:10 -1000

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-git_branch=$(git symbolic-ref --short -q HEAD)
+if [ -z "$TRAVIS_BRANCH" ]
+then
+  git_branch=$(git symbolic-ref --short -q HEAD)
+else
+  git_branch=$TRAVIS_BRANCH
+fi  
 echo "Git Branch: $git_branch"
-if [ $1 ]; then
+
+if [ $1 ]
+then
   filter=$1
 else
   filter='BEGIN'
 fi
 echo "Test Filter: $filter"
-if [ $git_branch = 'solutions' ]; then
+
+if [ $git_branch = 'solutions' ]
+then
   NODE_ENV=test ./node_modules/.bin/mocha --require ts-node/register ./tests/*.test.ts
 else 
   NODE_ENV=test ./node_modules/.bin/mocha --grep $filter --require ts-node/register ./tests/*.test.ts
