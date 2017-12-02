d9c9355025818931828707c51aa0deb7e2cc89ca
Michael North <michael.l.north@gmail.com>
2017-12-02 12:50:45 -0600

    assert for each property individually

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -10,14 +10,17 @@ export function validateRecordColumns(
   required: string[],
   forbidden: string[]
 ) {
-  assert.containsAllKeys(
-    record,
-    required,
-    `each ${opts.recordType} returned from ${opts.functionName} must have properties ${required.join(', ')}`
-  );
-  assert.doesNotHaveAnyKeys(
-    record,
-    forbidden,
-    `no ${opts.recordType} returned from ${opts.functionName} may have properties ${required.join(', ')}`
-  );
+  required.forEach(k => {
+    assert.isDefined(
+      (record as any)[k],
+      `each ${opts.recordType} returned from ${opts.functionName} must have property ${k}`
+    );
+  });
+
+  forbidden.forEach(k => {
+    assert.isUndefined(
+      (record as any)[k],
+      `no ${opts.recordType} returned from ${opts.functionName} may have properties ${k}`
+    );
+  });
 }
