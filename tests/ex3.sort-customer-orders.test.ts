08a1657c66b5cba824dac1eb030bcec8cfaeafd7
Michael North <michael.l.north@gmail.com>
2017-12-01 23:48:10 -0600

    Fix test

--- tests/ex3.sort-customer-orders.test.ts
+++ tests/ex3.sort-customer-orders.test.ts
@@ -1,23 +1,25 @@
 import { assert } from 'chai';
-import { sortBy } from 'lodash';
+import { orderBy } from 'lodash';
 import { slow, suite, test, timeout } from 'mocha-typescript';
 import { getCustomerOrders } from '../src/data/orders';
+import { logger } from '../src/log';
 
 @suite('EX3: "Customer Orders List" Query - Sort tests')
 class CustomerOrdersSortTest {
   @test('By default, order list is sorted ascending by ShippedDate')
   public async orderListDefaults() {
     let firstPageResult = await getCustomerOrders('ANTON', { perPage: 3 });
-    let sortedById = sortBy(firstPageResult, 'ShippedDate');
+
+    let sortedById = orderBy(firstPageResult, 'ShippedDate', 'asc');
     assert.deepEqual(firstPageResult, sortedById);
   }
 
   @test('using order="desc" (and specifying no column to sort on) sorts decending by ShippedDate')
   public async orderListDesc() {
     let firstPageResult = await getCustomerOrders('ANTON', { perPage: 3, order: 'desc' });
-    let sortedById = sortBy(firstPageResult, o => {
-      return -1 * new Date(o.ShippedDate).valueOf();
-    });
+    assert.containsAllKeys(firstPageResult[0], ['ShippedDate']);
+
+    let sortedById = orderBy(firstPageResult, 'ShippedDate', 'desc');
     assert.deepEqual(firstPageResult, sortedById);
   }
 }
