afe946ce07f371119883555092fb1aed6d63f5a2
Michael North <michael.l.north@gmail.com>
2017-12-02 21:14:02 -0600

    Update exercise 2 tests

--- tests/ex2.filtering-cutomers.test.ts
+++ tests/ex2.filtering-cutomers.test.ts
@@ -1,6 +1,9 @@
 import { assert } from 'chai';
 import { slow, suite, test, timeout } from 'mocha-typescript';
 import { getAllCustomers } from '../src/data/customers';
+import { validateRecordColumns } from 'tests/helpers';
+
+const ALL_CUSTOMERS_REQUIRED_COLS = ['Id', 'ContactName', 'CompanyName'];
 
 @suite('EX2: Filtering Customers')
 class EmployeeDataTest {
@@ -9,10 +12,11 @@ class EmployeeDataTest {
     let result = await getAllCustomers();
     assert.isArray(result, 'Expected result to be an array');
     assert.equal(result.length, 91, 'Expected 91 customers in array');
-    assert.hasAllKeys(
+    validateRecordColumns(
+      { recordType: 'customer', functionName: 'getAllCustomers' },
       result[3],
-      ['Id', 'ContactName', 'CompanyName'],
-      'each DB result has properties Id, ContactName and CompanyName'
+      ALL_CUSTOMERS_REQUIRED_COLS,
+      []
     );
   }
   @test('filter="fre" returns subset of results')
@@ -20,10 +24,11 @@ class EmployeeDataTest {
     let result = await getAllCustomers({ filter: 'fre' });
     assert.isArray(result, 'Expected result to be an array');
     assert.equal(result.length, 3, 'Expected 3 customers in array');
-    assert.hasAllKeys(
+    validateRecordColumns(
+      { recordType: 'customer', functionName: 'getAllCustomers({ filter: "fre" })' },
       result[1],
-      ['Id', 'ContactName', 'CompanyName'],
-      'each DB result has properties Id, ContactName and CompanyName'
+      ALL_CUSTOMERS_REQUIRED_COLS,
+      []
     );
   }
 
@@ -32,10 +37,11 @@ class EmployeeDataTest {
     let result = await getAllCustomers({ filter: 'mo' });
     assert.isArray(result, 'Expected result to be an array');
     assert.equal(result.length, 12, 'Expected 12 customers in array');
-    assert.hasAllKeys(
+    validateRecordColumns(
+      { recordType: 'customer', functionName: 'getAllCustomers({ filter: "mo" })' },
       result[1],
-      ['Id', 'ContactName', 'CompanyName'],
-      'each DB result has properties Id, ContactName and CompanyName'
+      ALL_CUSTOMERS_REQUIRED_COLS,
+      []
     );
   }
 }
