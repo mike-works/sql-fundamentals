87957c48b41717fcd6502fb742db21f6e1939bfc
Michael North <michael.l.north@gmail.com>
2018-01-26 00:35:12 -0800

    ✅ Exercise 13: Prepared Statements

--- src/data/orders.ts
+++ src/data/orders.ts
@@ -41,12 +41,12 @@ interface OrderCollectionOptions {
 
 const DEFAULT_ORDER_COLLECTION_OPTIONS: Readonly<
   OrderCollectionOptions
-  > = Object.freeze({
-    order: 'asc',
-    page: 1,
-    perPage: 20,
-    sort: 'id'
-  } as OrderCollectionOptions);
+> = Object.freeze({
+  order: 'asc',
+  page: 1,
+  perPage: 20,
+  sort: 'id'
+} as OrderCollectionOptions);
 
 export async function getAllOrders(
   opts: Partial<OrderCollectionOptions> = {}
@@ -110,23 +110,24 @@ WHERE o.customerid = $1
 
 export async function getOrder(id: string | number): Promise<Order> {
   const db = await getDb();
-  return await db.get(
-    sql`
-SELECT ${ORDER_COLUMNS.map(c => `o.${c}`).join(',')},
-  c.companyname as customername,
-  (e.firstname || ' ' || e.lastname) as employeename,
-  sum(od.unitprice * od.quantity) as subtotalprice
-FROM CustomerOrder as o
-LEFT JOIN Customer as c
-  ON o.customerid = c.id
-LEFT JOIN Employee as e
-  ON o.employeeid = e.id
-LEFT JOIN OrderDetail as od
-  ON od.orderid=o.id
-WHERE o.id=$1
-GROUP BY o.id, c.companyname, e.firstname, e.lastname`,
-    id
-  );
+  //   return await db.get(
+  //     sql`
+  // SELECT ${ORDER_COLUMNS.map(c => `o.${c}`).join(',')},
+  //   c.companyname as customername,
+  //   (e.firstname || ' ' || e.lastname) as employeename,
+  //   sum(od.unitprice * od.quantity) as subtotalprice
+  // FROM CustomerOrder as o
+  // LEFT JOIN Customer as c
+  //   ON o.customerid = c.id
+  // LEFT JOIN Employee as e
+  //   ON o.employeeid = e.id
+  // LEFT JOIN OrderDetail as od
+  //   ON od.orderid=o.id
+  // WHERE o.id=$1
+  // GROUP BY o.id, c.companyname, e.firstname, e.lastname`,
+  //     id
+  //   );
+  return await db.statements.getOrder.get(id);
 }
 
 export async function getOrderDetails(
--- src/db/prepared.ts
+++ src/db/prepared.ts
@@ -1,8 +1,28 @@
+import { ORDER_COLUMNS } from '../data/orders';
+import { sql } from '../sql-string';
 import { SQLDatabase, SQLStatement } from './db';
 
 export async function setupPreparedStatements<
   S extends SQLStatement,
   D extends SQLDatabase<S>
 >(db: D): Promise<{ [k: string]: S }> {
-  return {};
+  let getOrder = await db.prepare(
+    'getOrder',
+    sql`
+  SELECT ${ORDER_COLUMNS.map(c => `o.${c}`).join(',')},
+    c.companyname as customername,
+    (e.firstname || ' ' || e.lastname) as employeename,
+    sum(od.unitprice * od.quantity) as subtotalprice
+  FROM CustomerOrder as o
+  LEFT JOIN Customer as c
+    ON o.customerid = c.id
+  LEFT JOIN Employee as e
+    ON o.employeeid = e.id
+  LEFT JOIN OrderDetail as od
+    ON od.orderid=o.id
+  WHERE o.id = $1
+  GROUP BY o.id, c.companyname, e.firstname, e.lastname
+  `
+  );
+  return { getOrder };
 }
