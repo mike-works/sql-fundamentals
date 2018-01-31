cd8a0acde0fca2d99294e2033acc76a060d1715f
Michael North <michael.l.north@gmail.com>
2018-01-30 23:24:39 -0800

    Determine database type via environment var

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -3,11 +3,30 @@ import SQLiteDB from './sqlite-db';
 import PostgresDB from './postgres-db';
 import { SQLDatabase, SQLStatement } from 'src/db/db';
 
+enum DbType {
+  Postgres,
+  SQLite
+}
+
+const DB_TYPE: DbType = determineDbType();
+
+function determineDbType(): DbType {
+  switch ((typeof process.env.DB_TYPE || '').toLowerCase()) {
+    case 'postgres':
+      return DbType.Postgres;
+    default:
+      return DbType.SQLite;
+  }
+}
+
 export async function getDb(name: string): Promise<SQLDatabase<SQLStatement>> {
-  return await SQLiteDB.setup(name);
-  // return await PostgresDB.setup({
-  //   name,
-  //   host: 'localhost',
-  //   port: 5432
-  // });
+  if (DB_TYPE === DbType.Postgres) {
+    return await PostgresDB.setup({
+      name,
+      host: 'localhost',
+      port: 5432
+    });
+  } else {
+    return await SQLiteDB.setup(name);
+  }
 }
