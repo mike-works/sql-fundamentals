42ccbcb929df3e430f2fbfff709bd3b38468a868
Michael North <michael.l.north@gmail.com>
2018-01-30 23:27:16 -0800

    Log database type

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -2,6 +2,7 @@ import { sql } from '../sql-string';
 import SQLiteDB from './sqlite-db';
 import PostgresDB from './postgres-db';
 import { SQLDatabase, SQLStatement } from 'src/db/db';
+import { logger } from '../log';
 
 enum DbType {
   Postgres,
@@ -11,10 +12,12 @@ enum DbType {
 const DB_TYPE: DbType = determineDbType();
 
 function determineDbType(): DbType {
-  switch ((typeof process.env.DB_TYPE || '').toLowerCase()) {
+  switch ((process.env.DB_TYPE || '').trim().toLowerCase()) {
     case 'postgres':
+      logger.info('Database Type: PostgreSQL');
       return DbType.Postgres;
     default:
+      logger.info('Database Type: SQLite');
       return DbType.SQLite;
   }
 }
