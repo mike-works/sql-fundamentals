6a94535a1e3ca245be25a12e60554340f68b93fc
Michael North <michael.l.north@gmail.com>
2018-02-20 19:41:59 -0800

    ✅ Exercise 16: Full Text Search

---
+++ migrations/sqls/20180221031155-AddEx16FullTextSearch-down.sql
@@ -0,0 +1,4 @@
+DROP INDEX product_fts;
+DROP INDEX employee_fts;
+DROP INDEX customer_fts;
+DROP INDEX supplier_fts;
--- src/data/search.ts
+++ src/data/search.ts
@@ -4,19 +4,21 @@ import { sql } from '../sql-string';
 export async function getSearchResults(term: string): Promise<any[]> {
   let db = await getDb();
   return db.all(
-    sql`
-SELECT * from
-(SELECT 'product' as entity, productname as name, productname as the_text, ('' || id) as id FROM Product
-	UNION
-SELECT 'supplier' as entity, companyname as name, companyname as the_text, ('' || id) as id FROM Supplier
-	UNION
-SELECT 'customer' as entity, companyname as name, companyname as the_text, ('' || id) as id FROM Customer
- 	UNION
-SELECT 'category' as entity, categoryname as name, categoryname as the_text, ('' || id) as id FROM Category
- 	UNION
- SELECT 'employee' as entity, (firstname || ' ' || lastname) as name, (firstname || ' ' || lastname) as the_text, ('' || id) as id FROM Employee
-) as a
-WHERE lower(a.the_text) LIKE $1`,
-    `%${term}%`
+    sql`SELECT * FROM (SELECT 'product' as entity, productname as name, ('' || id) as id, ts_rank_cd(to_tsvector(productname || ' ' || quantityperunit), query, 32) AS rank
+FROM Product, to_tsquery($1) query
+WHERE to_tsvector(productname || ' ' || quantityperunit) @@ query
+UNION
+SELECT 'supplier' as entity, companyname as name, ('' || id) as id, ts_rank_cd(to_tsvector(contactname || ' ' || companyname || ' ' || country || ' ' || city), query, 32) AS rank
+FROM Supplier, to_tsquery($1) query
+WHERE to_tsvector(contactname || ' ' || companyname || ' ' || country || ' ' || city) @@ query
+UNION
+SELECT 'customer' as entity, companyname as name, ('' || id) as id, ts_rank_cd(to_tsvector(contactname || ' ' || companyname || ' ' || country || ' ' || city), query, 32) AS rank
+FROM Customer, to_tsquery($1) query
+WHERE to_tsvector(contactname || ' ' || companyname || ' ' || country || ' ' || city) @@ query
+UNION
+SELECT 'employee' as entity, (firstname || ' ' || lastname ) as name, ('' || id) as id, ts_rank_cd(to_tsvector(firstname || ' ' || lastname || ' ' || title || ' ' || notes), query, 32) AS rank
+FROM Employee, to_tsquery($1) query
+  WHERE to_tsvector(firstname || ' ' || lastname || ' ' || title || ' ' || notes) @@ query) as n ORDER BY rank`,
+    `${term}:*`
   );
 }
