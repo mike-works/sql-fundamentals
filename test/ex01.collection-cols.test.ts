a65f62b5efb606963bb5f345482193ca6eb7af88
Mike North <michael.l.north@gmail.com>
2018-03-21 17:04:13 -0700

    Improved EX1 tests

--- test/ex01.collection-cols.test.ts
+++ test/ex01.collection-cols.test.ts
@@ -5,6 +5,8 @@ import { getAllEmployees } from '../src/data/employees';
 import { getAllOrders } from '../src/data/orders';
 import { getAllProducts } from '../src/data/products';
 
+import { getAllCustomers } from '../src/data/customers';
+import { getAllSuppliers } from '../src/data/suppliers';
 import { validateRecordColumns } from './helpers/columns';
 import './helpers/global-hooks';
 
@@ -17,21 +19,16 @@ const REQUIRED_EMPLOYEE_LIST_COLS = [
   'title',
   'reportsto'
 ];
-const FORBIDDEN_EMPLOYEE_LIST_COLS = [
-  'titleofcourtesy',
-  'extension',
-  'photo',
-  'photopath'
-];
+const FORBIDDEN_EMPLOYEE_LIST_COLS = ['titleofcourtesy', 'extension', 'photo', 'photopath'];
 
-const REQUIRED_ORDER_LIST_COLS = [
-  'id',
-  'shipcity',
-  'shipcountry',
-  'employeeid',
-  'customerid'
-];
+const REQUIRED_ORDER_LIST_COLS = ['id', 'shipcity', 'shipcountry', 'employeeid', 'customerid'];
 const FORBIDDEN_ORDER_LIST_COLS = ['shipaddress'];
+
+const REQUIRED_SUPPLIER_LIST_COLS = ['id', 'contactname', 'companyname'];
+const FORBIDDEN_SUPPLIER_LIST_COLS = ['homepage'];
+
+const REQUIRED_CUSTOMER_LIST_COLS = ['id', 'contactname', 'companyname'];
+const FORBIDDEN_CUSTOMER_LIST_COLS = ['fax'];
 
 const REQUIRED_PRODUCT_LIST_COLS = [
   'id',
@@ -75,16 +72,38 @@ class EmployeeDataTest {
     );
   }
 
+  @test('All suppliers')
+  public async allSuppliers() {
+    let result = await getAllSuppliers();
+    assert.isArray(result, 'Expected result to be an array');
+    assert.isAbove(result.length, 20, 'Expected over 20 suppliers in array');
+    validateRecordColumns(
+      { recordType: 'supplier', functionName: 'getAllSuppliers' },
+      result[3],
+      REQUIRED_SUPPLIER_LIST_COLS,
+      FORBIDDEN_SUPPLIER_LIST_COLS
+    );
+  }
+
+  @test('All customers')
+  public async allCustomers() {
+    let result = await getAllCustomers();
+    assert.isArray(result, 'Expected result to be an array');
+    assert.isAbove(result.length, 20, 'Expected over 20 customers in array');
+    validateRecordColumns(
+      { recordType: 'customer', functionName: 'getAllCustomers' },
+      result[3],
+      REQUIRED_CUSTOMER_LIST_COLS,
+      FORBIDDEN_CUSTOMER_LIST_COLS
+    );
+  }
+
   @test('All products')
   public async allProducts() {
     let result = await getAllProducts();
     assert.isArray(result, 'Expected result to be an array');
     // TODO: tighten assertion
-    assert.isAtLeast(
-      result.length,
-      50,
-      'Expected at least 50 products in array'
-    );
+    assert.isAtLeast(result.length, 50, 'Expected at least 50 products in array');
     validateRecordColumns(
       { recordType: 'product', functionName: 'getAllProducts' },
       result[3],
